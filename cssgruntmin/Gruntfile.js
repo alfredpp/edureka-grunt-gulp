60b6fd341930b99e55d89b0510337d0ac97acdde
Alfred Paul <alfred.paul@mindtree.com>
2017-05-08 17:31:53 +0530

    Changing files.

--- cssgruntmin/Gruntfile.js
+++ cssgruntmin/Gruntfile.js
@@ -1,30 +1,13 @@
 module.exports = function(grunt) {
- 
-
-grunt.initConfig({
- 
-pkg: grunt.file.readJSON('package.json'),
- 
-
-cssmin: {
- 
-combine: {
- 
-files: {
- 
-'css/combined.css': ['css/custom.css', 'css/menu.css']
- 
-}
- 
-}
- 
-}
- 
-
-});
- 
-
-grunt.loadNpmTasks('grunt-contrib-cssmin');
- 
-
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    cssmin: {
+      combine: {
+        files: {
+          'css/combined.css': ['css/custom.css', 'css/menu.css']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 }
