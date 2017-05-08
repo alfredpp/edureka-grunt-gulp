787d9a9e38d1282fc424acc02e2d6c4c6133183d
Alfred Paul <alfred.paul@mindtree.com>
2017-05-08 17:38:12 +0530

    Changes

--- js-minify-grunt/Gruntfile.js
+++ js-minify-grunt/Gruntfile.js
@@ -1,26 +1,25 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        cssmin: {
-                 combine: {
-                           files: {
-                                  'css/combined.css': ['css/custom.css', 'css/menu.css']
-                                  }
-                          }
-                },
+    pkg: grunt.file.readJSON('package.json'),
+      cssmin: {
+        combine: {
+          files: {
+            'css/combined.css': ['css/custom.css', 'css/menu.css']
+          }
+        }
+      },
 
-        uglify: {
-                options: {
-                          mangle: false
-                         },
-                target: {
-                          files: {
-                         'js/combined.min.js': ['js/dropdown.js','js/util.js']
-                                 }
-                           }
-                }
-  });
+      uglify: {
+        options: {
+          mangle: false
+        },
+        target: {
+          files: {
+            'js/combined.min.js': ['js/dropdown.js','js/util.js']
+          }
+        }
+      }
+    });
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('default',['cssmin','uglify']);
